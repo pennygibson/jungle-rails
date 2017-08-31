5a75ec758c0df1b46391acedbc8d65e8fbeb9f41
pennygibson <cenneboulet@yahoo.ca>
2017-08-31 22:01:57 +0000

    review form created

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,10 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+  belongs_to :user
 
-  validates :product_id, presence true
-  validates :user_id, presence true
-  validates :rating, presence true
+  validates(:product_id, {presence: true})
+  validates :user_id, presence: true
+  validates :rating, presence: true
 
 
 end
